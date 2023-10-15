655386d001225e4bffb6dca1e28a42f5400651e4
josepabellana <josep.abe.pu@gmail.com>
2023-10-15 19:31:19 +0200

    Exercise 2.2

--- Poliformc.scala
+++ Poliformc.scala
@@ -8,3 +8,19 @@ def findFirst(ss: Array[String], key: String): Int = {
 }
 
 // inner call no stack created
+def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int =
+        if (n >= as.length) -1 
+        else if (p(as(n))) n 
+        else loop(n + 1)
+    loop(0)
+}
+def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int): Int =
+        if (n >= as.length -1) true
+        else if (!ordered(as(n), as(n+1))) false
+        else loop(n+1)
+    loop(0)
+}
