fd2a87e78e01cd4499302d83c5dff5267833fa87
josepabellana <josep.abe.pu@gmail.com>
2023-11-04 10:05:38 +0100

    Logistic Regression v1

---
+++ Classification ML/log_reg.scala
@@ -0,0 +1,26 @@
+import org.apache.spark.ml.classification.LogisticRegression
+
+import org.apache.spark.sql.SparkSession
+
+object LogisticRegressionWithElasticNetExample {
+
+    def main(): Unit = {
+        val spark = SparkSession
+            .builder
+            .appName("LogisticRegressionWithElasticNetExample")
+            .getOrCreate()
+
+        val training = spark.read.format("libsvm").load("sample_libsvm_data.txt")
+
+        val lr = new LogisticRegression()
+            .setMaxIter(10) // Max iterations
+            .setRegParam(0.3)
+            .setElasticNetParam(0.8)
+
+        val lrModel = lr.fit(training)
+
+        println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+
+        spark.stop()
+    }
+}
---
+++ Classification ML/readme.md
@@ -0,0 +1,18 @@
+The classification algorithm use its called Logistic Regression.
+
+Can read more in Sections 4-4.3 of Introduction of Statistical Learning by Gareth James.
+
+
+Some examples of classification problems:
+- Spam versus "Ham" emails
+- Loan Default(yes/no)
+- Cancer diagnosis
+
+Logistic regression is not predicting a continuos value until now.
+For this algorithm is like that the X axis is the threshold where users will be able to pay a loan or not.
+We can transform a linear regression line to a logistic regression curve
+
+The sigmoid(aa Logistic) Function takes in any value and utputs it to be between 0 and 1
+
+Formula = Beta(z) = 1 / (1 + e^-z)
+No matter the value you input of z its always going to return a value between 0 and 1.
